2636d9247cbdbc6300718cd1b02b13394a668a79
Mr.Ak <akhilsaivaraganti@gmail.com>
2021-01-25 14:31:28 +0530

    need to work on creating other pages

--- app.js
+++ app.js
@@ -1,33 +1,34 @@
-let menu = document.querySelector(".menu");
-let schlName = document.querySelector(".schlName");
-let tagLine = document.querySelector(".tagLine");
+let menu = document.querySelector(".menuBtn");
 
-function menuOpen(){
-    let aside = document.createElement("aside");
-    let body = document.querySelector("body");
-    body.appendChild(aside);
-    aside.classList.add("aside");
-    menu.style.display = "none";
-
-    addingElementsToAside(aside);
+function menuOpen() {
+  let aside = document.createElement("aside");
+  let body = document.querySelector("body");
+  body.appendChild(aside);
+  aside.classList.add("aside");
+  menu.style.display = "none";
+  addingElementsToAside(aside);
 }
 
-function addingElementsToAside(aside){
-    let crossBtn = document.createElement("button");
-    crossBtn.innerHTML = `<i class="lni lni-cross-circle"></i>`;
-    crossBtn.addEventListener("click", ()=>menuCollapse(aside));
-    crossBtn.classList.add("crossBtn");
-    aside.appendChild(crossBtn);
-    for(let i = 1 ; i <= 10 ; i++){
-        let paragraph = document.createElement("p");
-        paragraph.innerHTML = "class " + i;
-        paragraph.classList.add("classBox");
-        aside.appendChild(paragraph);
-    }
+function addingElementsToAside(aside) {
+  let crossBtn = document.createElement("button");
+  crossBtn.innerHTML = `<i class="lni lni-cross-circle"></i>`;
+  crossBtn.addEventListener("click", () => menuCollapse(aside));
+  crossBtn.classList.add("crossBtn");
+  aside.appendChild(crossBtn);
+  for (let i = 1; i <= 10; i++) {
+    let paragraph = document.createElement("p");
+    paragraph.innerHTML = "class " + i;
+    paragraph.classList.add("classBox");
+    paragraph.addEventListener("click", () => openNewPage());
+    aside.appendChild(paragraph);
+  }
 }
 
-function menuCollapse(aside){
-    aside.style.display = "none";
-    menu.style.display = "flex";
+function menuCollapse(aside) {
+  aside.style.display = "none";
+  menu.style.display = "flex";
 }
 
+function openNewPage() {
+  console.log("working");
+}
