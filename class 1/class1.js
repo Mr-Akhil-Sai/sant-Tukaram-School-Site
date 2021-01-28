4af55f67d02e4d7e0fdea9d6bc53f229747eef09
Mr.Ak <akhilsaivaraganti@gmail.com>
2021-01-28 14:43:11 +0530

    Implemented the new screens

--- class 1/class1.js
+++ class 1/class1.js
@@ -1,54 +1,64 @@
-let menu = document.querySelector(".menuBtn");
 let backBtn = document.querySelector(".backBtn");
 
-function menuOpen() {
-  let aside = document.createElement("aside");
-  let body = document.querySelector("body");
-  body.appendChild(aside);
-  aside.classList.add("asideClass1");
-  menu.style.display = "none";
-  addingCrossBtnAside(aside);
+let aside = document.createElement("aside");
+let body = document.querySelector("body");
+body.appendChild(aside);
+aside.classList.add("asideClass1");
+addingElementsToAside(aside);
+
+
+function addingElementsToAside(aside) {
+  let heading = document.createElement("h1");
+  let marati = document.createElement("p");
+  let hindi = document.createElement("p");
+  let english = document.createElement("p");
+  let maths = document.createElement("p");
+  let evs = document.createElement("p");
+  heading.innerHTML = "Class <br> &nbsp;&nbsp; 1";
+  marati.innerHTML = "Marati";
+  hindi.innerHTML = "Hindi";
+  english.innerHTML = "English";
+  maths.innerHTML = "Maths";
+  evs.innerHTML = "EVS";
+  addingStylesToAsideElements(
+    aside,
+    heading,
+    marati,
+    hindi,
+    english,
+    maths,
+    evs
+  );
 }
 
-function addingCrossBtnAside(aside) {
-  let crossBtn = document.createElement("button");
-  crossBtn.innerHTML = `<i class="lni lni-cross-circle"></i>`;
-  crossBtn.addEventListener("click", () => menuCollapse(aside));
-  crossBtn.classList.add("crossBtn");
-  aside.appendChild(crossBtn);
-  addingElementsToAside(aside);
-}
-
-function addingElementsToAside(aside){
-    let heading = document.createElement("h1");
-    let marati = document.createElement("p");
-    let hindi = document.createElement("p");
-    let english = document.createElement("p");
-    let maths = document.createElement("p");
-    let evs = document.createElement("p");
-    heading.innerHTML = "Class <br> &nbsp;&nbsp; 1";
-    marati.innerHTML = "Marati";
-    hindi.innerHTML = "Hindi";
-    english.innerHTML = "English";
-    maths.innerHTML = "Maths";
-    evs.innerHTML = "EVS";
-    addingStylesToAsideElements(aside, heading, marati, hindi, english, maths, evs);
-}
-
-function addingStylesToAsideElements(aside,heading, marati, hindi, english, maths, evs){
-    aside.appendChild(heading);
-    aside.appendChild(marati);
-    aside.appendChild(hindi);
-    aside.appendChild(english);
-    aside.appendChild(maths);
-    aside.appendChild(evs);
-    heading.classList.add("class1Heading");
-    marati.classList.add("marati");
-    hindi.classList.add("hindi");
-    english.classList.add("english");
-    maths.classList.add("maths");
-    evs.classList.add("evs");
-    marati.addEventListener("click", ()=>window.open("./marati.html", "_self") );
+function addingStylesToAsideElements(
+  aside,
+  heading,
+  marati,
+  hindi,
+  english,
+  maths,
+  evs
+) {
+  aside.appendChild(heading);
+  aside.appendChild(marati);
+  aside.appendChild(hindi);
+  aside.appendChild(english);
+  aside.appendChild(maths);
+  aside.appendChild(evs);
+  heading.classList.add("class1Heading");
+  marati.classList.add("marati");
+  hindi.classList.add("hindi");
+  english.classList.add("english");
+  maths.classList.add("maths");
+  evs.classList.add("evs");
+  marati.addEventListener("click", () => window.open("./marati.html", "_self"));
+  hindi.addEventListener("click", () => window.open("./hindi.html", "self"));
+  english.addEventListener("click", () =>
+    window.open("./english.html", "self")
+  );
+  maths.addEventListener("click", () => window.open("./maths.html", "self"));
+  evs.addEventListener("click", () => window.open("./evs.html", "self"));
 }
 
 function menuCollapse(aside) {
@@ -56,10 +66,10 @@ function menuCollapse(aside) {
   menu.style.display = "flex";
 }
 
-function homePage(){
+function homePage() {
   window.open("../index.html", "self");
 }
 
-function previousPage(){
+function previousPage() {
   window.open("./class1.html", "self");
 }
